e45c083d89b10694da5df8c26cd4ad713340efb1
Josh Black <joshblack@us.ibm.com>
2015-10-27 10:48:33 -0500

    Adding tests for props, context on stateless components

--- test/fixtures/plainStatelessComponent/actual.js
+++ test/fixtures/plainStatelessComponent/actual.js
@@ -1,7 +1,19 @@
 const StatelessComponentNoBlock = () => <div />;
+
 const StatelessComponentBlock = () => {
   return <div />;
 };
+
 const StatelessComponentBlockNoReturn = () => {
   <div />;
+};
+
+const StatelessComponentProps = (props) => <div {...props} />;
+
+const StatelessComponentDestrucProps = ({ foo }) => <div foo={foo} />;
+
+const StatelessWithContext = () => <div />;
+
+Stateless.contextTypes = {
+  foo: PropTypes.string
 };
--- test/fixtures/plainStatelessComponent/expected.js
+++ test/fixtures/plainStatelessComponent/expected.js
@@ -3,11 +3,33 @@
 var StatelessComponentNoBlock = function StatelessComponentNoBlock() {
   return React.createElement("div", null);
 };
+
 StatelessComponentNoBlock.displayName = "StatelessComponentNoBlock";
 var StatelessComponentBlock = function StatelessComponentBlock() {
   return React.createElement("div", null);
 };
+
 StatelessComponentBlock.displayName = "StatelessComponentBlock";
 var StatelessComponentBlockNoReturn = function StatelessComponentBlockNoReturn() {
   React.createElement("div", null);
+};
+
+var StatelessComponentProps = function StatelessComponentProps(props) {
+  return React.createElement("div", props);
+};
+
+StatelessComponentProps.displayName = "StatelessComponentProps";
+var StatelessComponentDestrucProps = function StatelessComponentDestrucProps(_ref) {
+  var foo = _ref.foo;
+  return React.createElement("div", { foo: foo });
+};
+
+StatelessComponentDestrucProps.displayName = "StatelessComponentDestrucProps";
+var StatelessWithContext = function StatelessWithContext() {
+  return React.createElement("div", null);
+};
+
+StatelessWithContext.displayName = "StatelessWithContext";
+Stateless.contextTypes = {
+  foo: PropTypes.string
 };
